407cb3f78a3d32cf54d04a4c32bf67f550c3fd3c
evertoncam7 <everton.camargo.cam7@gmail.com>
2021-08-06 01:15:44 -0300

    new layout

--- assets/js/bd.js
+++ assets/js/bd.js
@@ -1,12 +1,12 @@
 
 const bd = [
-    {id:1, title:"Ibitinga", desc:"qeqfeegegege", image:"assets/images/photos/1.jpg", arrayImages:[]},
-    {id:1, title:"dvdvev", desc:"qeqfeegegege", image:"assets/images/photos/2.jpg", arrayImages:[]},
-    {id:1, title:"rrrvrvrv", desc:"qeqfeegegege", image:"assets/images/photos/3.jpg", arrayImages:[]},
-    {id:1, title:"wdwdw", desc:"qeqfeegegege", image:"assets/images/photos/4.jpg", arrayImages:[]},
-    {id:1, title:"qeqf", desc:"qeqfeegegege", image:"assets/images/photos/5.jpg", arrayImages:[]},
-    {id:1, title:"fwfe", desc:"qeqfeegegegev", image:"assets/images/photos/6.jpg", arrayImages:[]},
-    {id:1, title:"eee", desc:"qeqfeegegege", image:"assets/images/photos/7.jpg", arrayImages:[]},
-    {id:1, title:"egeg", desc:"qeqfeegegege", image:"assets/images/photos/8.jpg", arrayImages:[]},
-    {id:1, title:"egeg", desc:"qeqfeegegege", image:"assets/images/photos/8.jpg", arrayImages:[]},
+    {id:1, title:"Ibitinga", desc:"qeqfeegegege", image:"assets/images/photos/1.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"dvdvev", desc:"qeqfeegegege", image:"assets/images/photos/2.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"rrrvrvrv", desc:"qeqfeegegege", image:"assets/images/photos/3.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"wdwdw", desc:"qeqfeegegege", image:"assets/images/photos/4.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"qeqf", desc:"qeqfeegegege", image:"assets/images/photos/5.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"fwfe", desc:"qeqfeegegegev", image:"assets/images/photos/6.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"eee", desc:"qeqfeegegege", image:"assets/images/photos/7.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"egeg", desc:"qeqfeegegege", image:"assets/images/photos/8.jpg", arrayImages:[], city:"", locale:""},
+    {id:1, title:"egeg", desc:"qeqfeegegege", image:"assets/images/photos/9.jpg", arrayImages:[], city:"", locale:""},
 ];
